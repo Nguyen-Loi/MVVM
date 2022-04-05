90ee404ceff61cfa746a26a7c08f2d88e37ee84e
NguyenLoi <hongloi123123@gmail.com>
2022-04-06 06:36:26 +0700

    84 add dependency injection instances app/di and main

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,4 +1,11 @@
 import 'package:advance_flutter/app/app_prefs.dart';
+import 'package:advance_flutter/data/data_source/remote_data_source.dart';
+import 'package:advance_flutter/data/network/app_api.dart';
+import 'package:advance_flutter/data/network/dio_factory.dart';
+import 'package:advance_flutter/data/network/network_info.dart';
+import 'package:advance_flutter/data/repository/repository_ipml.dart';
+import 'package:advance_flutter/domain/repository/repository.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,5 +18,26 @@ Future<void> initAppModule() async {
   instance.registerLazySingleton<SharedPreferences>(() => sharedPrefs);
 
   //App prefs instance
-  instance.registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+  instance
+      .registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+
+  //Network info
+  instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(DataConnectionChecker()));
+
+  //Dio factory
+  instance.registerLazySingleton<DioFactory>((() => DioFactory(instance())));
+
+  //App service alient
+  final dio = await instance<DioFactory>().getDio();
+  instance
+      .registerLazySingleton<AppServiceClient>((() => AppServiceClient(dio)));
+
+  //Remote data source
+  instance.registerLazySingleton<RemoteDataSource>(
+      (() => RemoteDataSourceImplementer(instance())));
+
+  //Repository
+  instance.registerLazySingleton<Repository>(
+      () => RepositoryImpl(instance(), instance()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:advance_flutter/app/app.dart';
+import 'package:advance_flutter/app/di.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await initAppModule();
+   runApp(MyApp());
+   }
 
