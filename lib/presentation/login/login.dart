7514b81740397d26032b23ad3e44171a4bc7eb0b
NguyenLoi <hongloi123123@gmail.com>
2022-04-05 10:26:46 +0700

    77. Login view UI

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,5 +1,9 @@
 import 'package:advance_flutter/presentation/login/login_viewmodel.dart';
+import 'package:advance_flutter/presentation/resources/assets_manager.dart';
+import 'package:advance_flutter/presentation/resources/color_manager.dart';
+import 'package:advance_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -11,8 +15,11 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   LoginViewModel _viewModel =
       LoginViewModel(null); // todo pass here login useCase
+
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start();
@@ -36,6 +43,31 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Container(
+      padding: EdgeInsets.only(top: AppPadding.p100),
+      color: ColorManager.white,
+      child: SingleChildScrollView(
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SvgPicture.asset(ImageAssets.loginIc),
+                  SizedBox(height: AppSize.s20),
+                  Padding(
+                      padding: EdgeInsets.only(
+                          left: AppPadding.p28, right: AppPadding.p28),
+                      child: StreamBuilder<bool>(
+                        stream: _viewModel.outputIsUserNameValid,
+                        builder: (context, snapshot) {
+                          return TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _userNameController,
+                          );
+                        },
+                      ))
+                ],
+              ))),
+    ));
   }
 }
