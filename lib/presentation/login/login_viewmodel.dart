2951d35d844aa643267cc8d58ee0804e40e4884f
NguyenLoi <hongloi123123@gmail.com>
2022-04-05 09:20:57 +0700

    73 Adding Data Classes to Login ViewModel  readme common/freezed_data_classes

--- lib/presentation/login/login_viewmodel.dart
+++ lib/presentation/login/login_viewmodel.dart
@@ -36,14 +36,12 @@ class LoginViewModel extends BaseViewModel
 
   @override
   setPassword(String password) {
-    // TODO: implement setPassword
-    throw UnimplementedError();
+    inputPassword.add(password);
   }
 
   @override
   setUserName(String userName) {
-    // TODO: implement setUserName
-    throw UnimplementedError();
+    inputUserName.add(userName);
   }
 
   // outputs
