6ea8e0d4f019824674efb09c71ee38622de012b7
NguyenLoi <hongloi123123@gmail.com>
2022-04-08 06:29:51 +0700

    99 Applying State Renderer Error State Empty State Content State common/state_render_ipml

--- lib/presentation/common/state_renderer/state_render_ipml.dart
+++ lib/presentation/common/state_renderer/state_render_ipml.dart
@@ -1,3 +1,4 @@
+import 'package:advance_flutter/data/mapper/mapper.dart';
 import 'package:advance_flutter/presentation/common/state_renderer/state_renderer.dart';
 import 'package:advance_flutter/presentation/resources/strings_manager.dart';
 
@@ -20,4 +21,49 @@ class LoadingState extends FlowState {
 
   @override
   StateRendererType getStateRendererType() => stateRendererType;
+}
+
+//Error state (POPUP, FULL SCREEN)
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+  ErrorState({
+    required this.stateRendererType,
+    String? message,
+  }) : message = message ?? AppStrings.loading;
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+
+// CONTENT STATE
+
+class ContentState extends FlowState {
+  ContentState();
+
+  @override
+  String getMessage() => EMPTY;
+
+  @override
+  StateRendererType getStateRendererType() =>
+      StateRendererType.CONTENT_SCREEN_STATE;
+}
+
+// EMPTY STATE
+
+class EmptyState extends FlowState {
+  String message;
+
+  EmptyState(this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() =>
+      StateRendererType.EMPTY_SCREEN_STATE;
 }
